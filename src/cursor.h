c8dad610e5a43f79f77e14db2ce41eacdc05f794
Jonathan Wheelhouse <jonathan.wheelhouse@gmail.com>
2018-01-20 12:32:53 +1100

    Make the create cursor functions static
    These functions should only be visible to cursor.c; they should not be
    global.

--- src/cursor.h
+++ src/cursor.h
@@ -12,12 +12,6 @@ struct cursors
 	SDL_Cursor *cursor_wheelhouse_black_with_white_lines;
 };
 
-SDL_Cursor *create_cursor_arrow(void);
-SDL_Cursor *create_wheelhouse_cursor(void);
-SDL_Cursor *create_wheelhouse_cursor_transparent(void);
-SDL_Cursor *create_wheelhouse_cursor_inverted(void);
-SDL_Cursor *create_wheelhouse_cursor_black_with_white_lines(void);
-
 struct cursors *setup_cursors(void);
 
 #endif
